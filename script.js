b3038334bec97a74d8a7ba716cf863bd1a591b52
assisMeneghetti <assismfabri@hotmail.com>
2021-08-25 18:52:19 -0300

    finaliza requisito 3

---
+++ script.js
@@ -0,0 +1,12 @@
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const btnEnter = document.getElementById('btn-enter');
+function validation() {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+btnEnter.addEventListener('click', validation);
