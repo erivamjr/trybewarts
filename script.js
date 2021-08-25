74393d02be72956be78ecab7c8e01e4a6dc44aa0
assisMeneghetti <assismfabri@hotmail.com>
2021-08-25 19:27:35 -0300

    inicia requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const btnEnter = document.getElementById('btn-enter');
+const btnSubmit = document.getElementById('submit-btn');
+const agreeInput = document.querySelector('#agreement');
 function validation() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
@@ -10,4 +12,12 @@ function validation() {
 }
 
 btnEnter.addEventListener('click', validation);
-// test
+
+if (agreeInput.checked === true) {
+  btnSubmit.addEventListener('click', validationSubmit);
+}
+
+function validationSubmit() {
+  console.log('Passei aqui');
+}
+
