dea94ae241bb4a2930b25192086f12f31302bed7
José Erivam <erivamdev@gmail.com>
2021-08-26 16:33:54 -0300

    Adicionando requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const btnEnter = document.getElementById('btn-enter');
+const btnSubmit = document.getElementById('submit-btn');
+const agreeInput = document.getElementById('agreement');
 
 function validation() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -11,3 +13,17 @@ function validation() {
 }
 
 btnEnter.addEventListener('click', validation);
+
+// Requisito 18
+
+btnSubmit.disabled = true;
+function validationSubmit() {
+  if (agreeInput.checked === true) {
+    console.log(agreeInput.checked);
+    btnSubmit.disabled = false;
+  } else {
+    console.log(agreeInput.checked);
+    btnSubmit.disabled = true;
+  }
+}
+agreeInput.addEventListener('click', validationSubmit);
