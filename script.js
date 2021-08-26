6fb49456fb2ecc32eafe66ce2a7c50274f1482e9
assisMeneghetti <assismfabri@hotmail.com>
2021-08-26 15:02:27 -0300

    adiciona requisito 20

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const btnEnter = document.getElementById('btn-enter');
+const textArea = document.getElementById('textarea');
+const counterDisplay = document.querySelector('#counter');
 
 function validation() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -11,3 +13,9 @@ function validation() {
 }
 
 btnEnter.addEventListener('click', validation);
+
+textArea.addEventListener('keyup', () => {
+  const maxLength = 500;
+  const counter = maxLength - textArea.value.length;
+  counterDisplay.innerHTML = counter;
+});
