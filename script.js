263e3d5c8d5c346382a9fe9ec91a29fff394a545
José Erivam <erivamdev@gmail.com>
2021-08-27 14:44:53 -0300

    Adicionando Requisito 21

--- script.js
+++ script.js
@@ -3,21 +3,17 @@ const password = document.getElementById('password');
 const btnEnter = document.getElementById('btn-enter');
 const btnSubmit = document.getElementById('submit-btn');
 const agreeInput = document.getElementById('agreement');
+
 const textArea = document.getElementById('textarea');
 const counterDisplay = document.getElementById('counter');
-const btnSubmit = document.getElementById('submit-btn');
+
 const name = document.getElementById('input-name');
 const lastName = document.getElementById('input-lastname');
 const house = document.getElementById('house');
 const subjects = document.getElementsByClassName('subject');
-const rate = document.getElementsByName('rate');
-const family = document.querySelectorAll('#label-family input:checked');
-const agree = document.getElementById('agreement');
-const form = document.getElementById('evaluation-form');
-const familyChecked = document.querySelector('input[type=radio]:checked');
 
-console.log(family);
-// console.log(familyChecked, form, email, agree.value, name, house.value, family[0].value, subjects[0].value, rate[0].value);
+const form = document.getElementById('evaluation-form');
+let selected = '';
 
 function validation() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -29,32 +25,43 @@ function validation() {
 
 btnEnter.addEventListener('click', validation);
 
-textArea.addEventListener('input', () => {
-  // Requisito-18
+// Requisito-18
 
-  btnSubmit.disabled = true;
-  function validationSubmit() {
-    if (agreeInput.checked === true) {
-      console.log(agreeInput.checked);
-      btnSubmit.disabled = false;
-    } else {
-      console.log(agreeInput.checked);
-      btnSubmit.disabled = true;
-    }
+btnSubmit.disabled = true;
+function validationSubmit() {
+  if (agreeInput.checked === true) {
+    console.log(agreeInput.checked);
+    btnSubmit.disabled = false;
+  } else {
+    console.log(agreeInput.checked);
+    btnSubmit.disabled = true;
   }
-  agreeInput.addEventListener('click', validationSubmit);
-
+}
+agreeInput.addEventListener('click', validationSubmit);
+textArea.addEventListener('input', () => {
   textArea.addEventListener('keyup', () => {
     const maxLength = 500;
     const counter = maxLength - textArea.value.length;
     counterDisplay.innerHTML = counter;
   });
+});
 
-  btnSubmit.addEventListener('click', (event) => {
-    event.preventDefault();
-    form.innerHTML = '';
+btnSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+  const familyChecked = document.querySelector('input[name=family]:checked');
+  const rate = document.querySelector('input[name=rate]:checked');
+  for (let key = 0; key < subjects.length; key += 1) {
+    if (subjects[key].checked) {
+      selected = `${selected + subjects[key].value} `;
+    }
+  }
+  const arrayInformation = [`Nome: ${name.value} ${lastName.value}`, `Email: ${email.value}`,
+    `Casa: ${house.value}`, `Família: ${familyChecked.value}`, `Matérias: ${selected}`,
+    `Avaliação: ${rate.value}`, `Observações: ${textArea.value}`];
+  form.innerHTML = '';
+  for (let key = 0; key < arrayInformation.length; key += 1) {
     const p = document.createElement('p');
-    p.innerHTML = `Nome: ${name.value} ${lastName.value};
-  Email: ${email.value} Casa: ${house.value} Família: ${familyChecked};`
+    p.innerHTML = arrayInformation[key];
     form.appendChild(p);
-  });
+  }
+});
